5a670f74e05a39674a8682143ecb4f8edf491661
Furi Sherpa Lama <lamafuri60@gmail.com>
2026-02-02 21:20:23 +0545

    make patient properties (age , mobile , postcode from private to public : Patient

--- Patient.py
+++ Patient.py
@@ -14,15 +14,14 @@ class Patient(Person):
 
         #ToDo1
         super().__init__(first_name , surname)
-        self.__age = age
-        self.__mobile = mobile
-        self.__postcode = postcode
+        self.age = age
+        self.mobile = mobile
+        self.postcode = postcode
         self.__doctor = 'None'
 
     def get_doctor(self) :
         #ToDo3
         return self.__doctor
-
     def link(self, doctor):
         """Args: doctor(string): the doctor full name"""
         self.__doctor = doctor
@@ -35,7 +34,7 @@ class Patient(Person):
     # For saving to a text file
     def to_csv_format(self):
         """First Name , Surname , Age , Mobile , Post Code , Doctor , symptom 1|symptom 2|symptom 3"""
-        return f"{self.get_first_name()},{self.get_surname()},{self.__age},{self.__mobile},{self.__postcode},{self.__doctor}"
+        return f"{self.get_first_name()},{self.get_surname()},{self.age},{self.mobile},{self.postcode},{self.__doctor}"
 
     def __str__(self):
-        return f'{self.full_name():^30}|{self.__doctor:^30}|{self.__age:^5}|{self.__mobile:^15}|{self.__postcode:^10}'
+        return f'{self.full_name():^30}|{self.__doctor:^30}|{self.age:^5}|{self.mobile:^15}|{self.postcode:^10}'
